ae569022c47906eebd13c2049ea658f923912d7e
Paulo Estrela <paulo.jaime.estrela@gmail.com>
2020-11-10 17:39:46 +0000

    1 commit master

---
+++ X3laTest/src/com/test/X3laTestClss.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class X3laTestClss {
+
+	public static void main(String[] args) {
+		X3laUtils.printMsg("Master - 1 commit");
+	}
+
+}
---
+++ X3laTest/src/com/test/X3laUtils.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class X3laUtils {
+
+	public static void printMsg(String msg) {
+		System.out.println(msg);
+	}
+
+}
