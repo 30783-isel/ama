a69c9458a9d19fab1b24baf5e2e0711099128be5
Paulo Estrela <paulo.jaime.estrela@gmail.com>
2020-11-10 18:03:54 +0000

    sourcetree - 2

--- X3laTest/src/com/test/X3laTestClss.java
+++ X3laTest/src/com/test/X3laTestClss.java
@@ -5,6 +5,7 @@ public class X3laTestClss {
 	public static void main(String[] args) {
 		X3laUtils.printMsg("Master - 1 commit");
 		X3laUtils.printMsg("Sourcetree - 1 commit");
+		X3laUtils.printMsg("Sourcetree - 2 commit");
 	}
 
 }
