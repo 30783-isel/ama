1f0238918883c1ff0c56f4014a8062c72e389102
Paulo Estrela <paulo.jaime.estrela@gmail.com>
2020-11-11 22:33:39 +0000

    1 commit tortoise 2 #	X3laTest/src/com/test/X3laTestClss.java

--- X3laTest/src/com/test/X3laTestClss.java
+++ X3laTest/src/com/test/X3laTestClss.java
@@ -4,11 +4,6 @@ public class X3laTestClss {
 
 	public static void main(String[] args) {
 		X3laUtils.printMsg("Master - 1 commit");
-		X3laUtils.printMsg("Sourcetree - 1 commit");
-		X3laUtils.printMsg("Sourcetree - 2 commit");
-		X3laUtils.printMsg("Sourcetree - 3 commit");
 		X3laUtils.printMsg("Tortoise - 1 commit");
-		X3laUtils.printMsg("Tortoise - 2 commit");
-		X3laUtils.printMsg("Tortoise - 3 commit");
 	}
 }
